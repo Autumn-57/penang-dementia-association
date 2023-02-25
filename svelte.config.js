253f5586b115174a69f6a416935a9f2971ccbbf1
Autumn-57 (last name) <penangmapper15@gmail.com>
2023-02-25 07:35:18 +0000

    vercel deploy 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
 import vercel from '@sveltejs/adapter-vercel';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-   	 // hydrate the <div id="svelte"> element in src/app.html
-   	 target: '#svelte',
-   	 adapter: vercel()
-    }
+	kit: {
+   	 	adapter: vercel()
+	}
 };
 
 export default config;
