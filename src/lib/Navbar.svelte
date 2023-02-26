4df6020590e19d96b8a4c19b039f4216853d4a89
Autumn-57 (last name) <penangmapper15@gmail.com>
2023-02-26 01:29:51 +0000

    navbar

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <span class="navbar-text">
+           Penang Dementia Association
+        </span>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <button class="nav-link">Home</button>
+          <button class="nav-link">Features</button>
+          <button class="nav-link">Pricing</button>
+          <button class="nav-link">Disabled</button>
+        </div>
+      </div>
+    </div>
+  </nav>
